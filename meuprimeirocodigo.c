b0d1db55656a394ae75c5772b2cbc311afa826e0
AndreC003 <andrecarvalhoxn@gmail.com>
2024-09-16 15:26:32 +0100

    Add files via upload

---
+++ meuprimeirocodigo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+char Character;
+
+printf(" Introduza o caractere: \t");
+scanf("%c", &Character);
+
+    printf("                    ****           \n"); 
+    printf("                    ****           \n"); 
+    printf("                    *              \n"); 
+    printf("                   ***             \n");
+    printf("                  *****            \n");
+    printf("                 *******           \n");
+    printf("           ********************    \n");
+    printf("              **************       \n");
+    printf("               ***********         \n");
+ 
+
+    return 0;
+}
