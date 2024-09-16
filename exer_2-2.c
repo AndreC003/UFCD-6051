f54d13cecf4f74b9859ac83ca27bc8aa1e0b991a
AndreC003 <andrecarvalhoxn@gmail.com>
2024-09-16 14:53:13 +0100

    Add files via upload

---
+++ exer_2-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int x = 0;
+int y = 0;
+
+printf(" Introduza o x : \t");
+scanf("%d", &x);
+
+printf(" Introduza o y : \t");
+scanf("%d", &y);
+
+int sum = x + y;
+
+printf("sum: %d", sum);
+
+
+
+
+}
